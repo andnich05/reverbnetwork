59f84221cf653e989f4a9336180dc57607c74add
Andrej <Andrej@cc7b295b-de7c-f845-9ddc-318a10e2e77b>
2015-07-03 09:58:11 +0000

    - Derived class from IMessage added but without use (yet)

---
+++ source/ComponentMessage.cpp
@@ -0,0 +1,31 @@
+#include "ComponentMessage.h"
+
+
+namespace Steinberg {
+namespace Vst {
+	ComponentMessage::ComponentMessage()
+		: message(nullptr)
+	{
+	}
+
+	ComponentMessage::~ComponentMessage()
+	{
+	}
+
+	FIDString PLUGIN_API ComponentMessage::getMessageID() {
+		FIDString bla = "bla";
+		return bla;
+	}
+
+	void PLUGIN_API ComponentMessage::setMessageID(FIDString id) {
+
+	}
+
+	IAttributeList* PLUGIN_API ComponentMessage::getAttributes() {
+		if (!attributeList)
+			attributeList = new HostAttributeList;
+		return attributeList;
+	}
+
+}
+}
---
+++ source/ComponentMessage.h
@@ -0,0 +1,40 @@
+#ifndef COMPONENTMESSAGE_H
+#define COMPONENTMESSAGE_H
+
+#include "..\pluginterfaces\vst\ivstmessage.h"
+#include "public.sdk\source\vst\hosting\hostclasses.h"
+
+namespace Steinberg {
+namespace Vst {
+
+class ComponentMessage : public IMessage {
+
+public:
+
+	ComponentMessage::ComponentMessage();
+	ComponentMessage::~ComponentMessage();
+
+	/** Returns the message ID (for example "TextMessage"). */
+	virtual FIDString PLUGIN_API getMessageID();
+
+	/** Sets a message ID (for example "TextMessage"). */
+	virtual void PLUGIN_API setMessageID(FIDString id /*in*/);
+
+	/** Returns the attribute list associated to the message. */
+	virtual IAttributeList* PLUGIN_API getAttributes();
+
+
+
+	virtual inline void setMessagePointer(void* messagePtr) { message = messagePtr; }
+	virtual inline void* getMessagePointer() { if(message) return message; }
+
+	HostAttributeList* attributeList;
+	void* message;
+
+private:
+	
+
+};
+}}
+
+#endif // COMPONENTMESSAGE_H
