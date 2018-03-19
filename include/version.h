117e339ce8580f56fc9b4dc2a789a8f5ecf8742f
andnich05 <andnich05dev@gmail.com>
2018-03-19 21:49:58 +0100

    Updated version number to 1.0.1.0

--- include/version.h
+++ include/version.h
@@ -67,11 +67,11 @@
 #define SUB_VERSION_STR "0"
 #define SUB_VERSION_INT 0
 
-#define RELEASE_NUMBER_STR "0"
-#define RELEASE_NUMBER_INT 0
+#define RELEASE_NUMBER_STR "1"
+#define RELEASE_NUMBER_INT 1
 
-#define BUILD_NUMBER_STR "99" // Build number to be sure that each result could identified.
-#define BUILD_NUMBER_INT 99
+#define BUILD_NUMBER_STR "0" // Build number to be sure that each result could identified.
+#define BUILD_NUMBER_INT 0
 
 // Version with build number (example "1.0.3.342")
 #define FULL_VERSION_STR MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR "." BUILD_NUMBER_STR
